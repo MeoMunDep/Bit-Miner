be39acba882285edeab01dd8d663f6af4e2dfb6a
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2024-12-25 19:21:22 +0700

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
 @echo off
-title name Bot
+title Bitminer Bot
 color 0A
 
 cd ..
@@ -14,7 +14,7 @@ if exist node_modules (
 :MENU
 cls
 echo =================================================================
-echo    name BOT SETUP AND RUN SCRIPT
+echo    Bitminer BOT SETUP AND RUN SCRIPT
 echo =================================================================
 echo.
 echo Current directory: %CD%
@@ -55,19 +55,15 @@ echo Creating configuration files...
 
 if not exist configs.json (
     echo {> configs.json
-    echo   "timeZone": "en-US",>> configs.json
-    echo   "rotateProxy": false,>> configs.json
-    echo   "skipInvalidProxy": false,>> configs.json
-    echo   "proxyRotationInterval": 2,>> configs.json
+    echo   "country_time": "en-US",>> configs.json
+    echo   "howManyLevelsUpgrade": 2,>> configs.json
     echo   "delayEachAccount": [5, 8],>> configs.json
-    echo   "timeToRestartAllAccounts": 300,>> configs.json
-    echo   "howManyAccountsRunInOneTime": 100,>> configs.json
-    echo   "doTasks": true,>> configs.json
-    echo   "playGames": true,>> configs.json
-    echo   "referralCode": "">> configs.json
+    echo   "countdown": 300,>> configs.json
+    echo   "limit": 100,>> configs.json
     echo }>> configs.json
     echo Created configs.json
 )
+
 
 if not exist datas.txt (
     type nul > datas.txt
